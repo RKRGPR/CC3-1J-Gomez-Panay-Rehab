d116bfdc03463e7f49456696b0d95177edea79a4
RKRGPR <46713057+RKRGPR@users.noreply.github.com>
2019-01-28 13:56:49 +0800

    Add files via upload
    Renz Rallion Gomez, Kriezter Rey Fel Panay & Ali Hussein Rehab
    CITCS 1J
    
    Activity 2: Student Info
    "Act2StudentInfo.java" contains the main method.
    "StudentInfo.java" contains the private and public fields, constructor, accessors and mutators.

---
+++ Act2StudentInfo.java
@@ -0,0 +1,31 @@
+package act2studentinfo;
+public class Act2StudentInfo {
+    public static void main(String[] args) {
+        //object gomez
+        StudentInfo gomez = new StudentInfo("CITCS", "2nd Term", "S.Y. 2018-2019");
+        gomez.setIdNumber("18-4164-797");
+        gomez.setFirstName("Renz Rallion");
+        gomez.setLastName("Gomez");
+        gomez.setCourse("BSCS");
+        gomez.setMajor("Digital Arts and Animation");
+        gomez.print();
+        //object rehab
+        StudentInfo rehab = new StudentInfo("CITCS", "2nd Term", "S.Y. 2018-2019");
+        rehab.setIdNumber("18-5805-517");
+        rehab.setFirstName("Ali Hussein");
+        rehab.setLastName("Rehab");
+        rehab.setCourse("BSIT");
+        rehab.setMajor("Network and Security");
+        rehab.print();
+
+        //object panay
+        StudentInfo panay = new StudentInfo("CITCS", "2nd Term", "S.Y. 2018-2019");
+        panay.setIdNumber("18-4930-451");
+        panay.setFirstName("Kriezter Rey Fel");
+        panay.setLastName("Panay");
+        panay.setCourse("BSIT");
+        panay.setMajor("Network and Security");
+        panay.print();
+    }
+    
+}
---
+++ StudentInfo.java
@@ -0,0 +1,53 @@
+package act2studentinfo;
+public class StudentInfo {
+    //fields
+    private String idNumber, firstName, lastName, course, major;
+    public String college, term, year;
+    //constructor
+    public StudentInfo(String college, String term, String year){
+        this.college = college;
+        this.term = term;
+        this.year = year;
+    }
+    //accessors and mutators
+    public String getIdNumber(){
+        return idNumber;
+    }
+    public void setIdNumber(String idNumber){
+        this.idNumber = idNumber;
+    }
+    public String getfirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public void setCourse(String course){
+        this.course = course;
+    }
+    public String getMajor(){
+        return major;
+    }
+    public void setMajor(String major){
+        this.major = major;
+    }
+    //print
+    public void print(){
+        System.out.println("\nUniversity of the Cordilleras");
+        System.out.println(college);
+        System.out.println(term+", "+year);
+        System.out.println("Student Info:");
+        System.out.println("ID Number: " + idNumber);
+        System.out.println("Name: "+ lastName+", "+firstName);
+        System.out.println("Academic Program: " + course+" - "+major+" Track");
+    }
+}
